b7c09ccc5c6762bf5ea6ed5c16000124d90474cf
abhi <abhijeet.patil@crest.in>
2016-05-26 16:05:02 +0530

    First Commit : A sample journey with phantomjs

---
+++ src/test/java/protocols/features/search/SimpleSearchByKeywordStory.java
@@ -0,0 +1,47 @@
+package protocols.features.search;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.questions.page.TheWebPage;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import protocols.tasks.OpenAdisApplication;
+import protocols.tasks.OpenTheApplication;
+import protocols.tasks.Search;
+
+import static net.serenitybdd.screenplay.EventualConsequence.eventually;
+import static net.serenitybdd.screenplay.GivenWhenThen.*;
+import static org.hamcrest.Matchers.containsString;
+
+@RunWith(SerenityRunner.class)
+public class SimpleSearchByKeywordStory {
+
+    Actor anna = Actor.named("Anna");
+
+    @Managed(uniqueSession = true,driver = "phantomjs")
+    public WebDriver herBrowser;
+
+    @Steps
+    OpenAdisApplication openAdisApplication;
+
+    @Before
+    public void annaCanBrowseTheWeb() {
+        anna.can(BrowseTheWeb.with(herBrowser));
+    }
+
+    @Test
+    public void seach_results_should_show_the_search_term_in_the_title() {
+
+        givenThat(anna).wasAbleTo(openAdisApplication);
+
+        when(anna).attemptsTo(Search.forTheTerm("BDD In Action"));
+
+        then(anna).should(eventually(seeThat(TheWebPage.title(), containsString("BDD In Action"))));
+
+    }
+}
---
+++ src/test/java/protocols/tasks/OpenAdisApplication.java
@@ -0,0 +1,13 @@
+package protocols.tasks;
+
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+
+public class OpenAdisApplication implements Task{
+
+    @Override
+    public <T extends Actor> void performAs(T t) {
+
+    }
+}
